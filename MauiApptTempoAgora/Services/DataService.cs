85268d70646f497639ee6fa8ff43d7de7f8660e5
231145 <231145@CPD03-M17>
2025-04-07 10:57:47 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApptTempoAgora/Services/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiApptTempoAgora.Models;
+using MetalPerformanceShaders;
+
+namespace MauiApptTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = $"https://api.openweathermap.org"
+        }
+    }
+}
