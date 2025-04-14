f2ed659ee781af3441e06e92702a4ee32ee294d4
231145 <231145@CPD03-M17>
2025-04-14 10:52:04 -0300

    kkdkdkdkd

--- MauiApptTempoAgora/Services/DataService.cs
+++ MauiApptTempoAgora/Services/DataService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MauiApptTempoAgora.Models;
-using MetalPerformanceShaders;
+using Newtonsoft.Json.Linq;
 
 namespace MauiApptTempoAgora.Services
 {
@@ -14,7 +14,39 @@ namespace MauiApptTempoAgora.Services
         {
             Tempo? t = null;
 
-            string chave = $"https://api.openweathermap.org"
+            string chave = $"6135072afe7f6cec1537d5cb08a5a1a2";
+
+            string url = $"https://api.openweatermap.org/data/2.5/weather?" + $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(); 
+
+                if(resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();   
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["description"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                    };
+                }
+            }
         }
     }
 }
